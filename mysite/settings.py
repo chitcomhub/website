ea5b016d992046d26231580e59a1650e4ab70663
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-28 18:31:31 +0300

    create home app

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chiters',
+    'home.apps.HomeConfig',
+    'chiters.apps.ChitersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
