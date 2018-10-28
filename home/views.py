b7b1af25a9dc71965a3d8ab0135ea167b7e93eec
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-28 21:31:25 +0300

    add home template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('Приложение Home работает')
+	return render(request, 'home/index.html')
