04e90705a31590bdf1059523b97f47bb6ea8cb33
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-30 13:17:34 +0300

    add chiters html table in chiter app

--- chiters/urls.py
+++ chiters/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'chiters'
 urlpatterns = [
 	path('', views.chiters, name='chiters'),
+	path('register/', views.register, name='register'),
 	path('message/', views.message, name='message'),
 ]
--- chiters/views.py
+++ chiters/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import redirect, render
 from .forms import PostForm
 
+from .models import Chiter
+
 def chiters(request):
+	chiter_list = Chiter.objects.order_by('-date')
+	context = {'chiter_list': chiter_list}
+	return render(request, 'chiters/chiters.html', context)
+
+def register(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
