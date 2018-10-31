686d013fb38de23191ff29e627a24ff3814a239b
azamatmurdalov <azamat.murdalov@mail.ru>
2018-10-31 23:28:44 +0300

    add html pages in home app

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('contact/', views.contact, name='contact'),
+	path('about/', views.about, name='about'),
+	path('works/', views.works, name='works'),
 ]
