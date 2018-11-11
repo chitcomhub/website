643717ce2a523a1262a2413d23b620fed32c80fd
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-11 23:54:05 +0300

    use generic view

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 app_name = 'news'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:post_id>/', views.post, name='post'),
+	path('', views.IndexView.as_view(), name='index'),
+	path('<int:pk>/', views.PostView.as_view(), name='post'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
