3a19dea1a81e41268ca645e5f56351417e0f79a9
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-21 23:52:47 +0300

    added the ability to: add/update/delete news

--- news/models.py
+++ news/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
 	title = models.CharField('Заголовок', max_length=200)
 	context = models.TextField('Описание')
-	datetime = models.DateTimeField('Дата публикации')
-	pic = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='Изображение', help_text='Ширина изображения должна быть 800px')
+	datetime = models.DateTimeField('Дата публикации', default=timezone.now)
+	pic = models.ImageField(null=True, blank=True, upload_to='images/', verbose_name='Изображение', help_text='Ширина и высота 800x400px')
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('news:post-detail', kwargs={'pk': self.pk})
