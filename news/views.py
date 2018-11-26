156393ed31afe72e996af81f831a36aa36752748
ai-rage <azamat.murdalov@mail.ru>
2018-11-26 23:12:50 +0300

    fix column 'content' in news model

--- news/views.py
+++ news/views.py
@@ -36,7 +36,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
-	fields = ['title', 'context']
+	fields = ['title', 'content']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
@@ -45,7 +45,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
-	fields = ['title', 'context']
+	fields = ['title', 'content']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
