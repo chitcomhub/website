fff3ba982a92ca3f38c29765bbb72d452f32dfaf
ai-rage <azamat.murdalov@mail.ru>
2018-12-04 23:59:00 +0300

    add article create form in paperchit

--- paperchit/urls.py
+++ paperchit/urls.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import (
+	ArticleListView,
+	ArticleDetailView,
+	ArticleCreateView
+)
+
 
 app_name = 'paperchit'
 urlpatterns = [
 	path('', ArticleListView.as_view(), name='index'),
 	path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+	path('new/', ArticleCreateView.as_view(), name='new-article')
 ]
 
 if settings.DEBUG:
