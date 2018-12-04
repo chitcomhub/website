ca395e3dc71f7681da4a372616c66ca09e63b95a
ai-rage <azamat.murdalov@mail.ru>
2018-12-05 00:47:31 +0300

    update paperchit app

--- paperchit/models.py
+++ paperchit/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Article(models.Model):
 	title = models.CharField('Заголовок', max_length=200)
@@ -11,3 +12,6 @@ class Article(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('paperchit:article-detail', kwargs={'pk': self.pk})
--- paperchit/views.py
+++ paperchit/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView
+)
 from .models import Article
 
 
@@ -12,3 +17,12 @@ class ArticleListView(ListView):
 
 class ArticleDetailView(DetailView):
 	model = Article
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+	model = Article
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
